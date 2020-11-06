8318db8ed5f70223884044746e124d7eaacc2827
Mark Huang <huangjc@rock-chips.com>
2020-11-06 08:30:37 +0800

    Update to v1.21,optimize uvc frame for ipc
    Change-Id: I3b24706b6db69fd326905876f43af6604a738a08
    Signed-off-by: Mark Huang <huangjc@rock-chips.com>

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ int uvc_app_log_level = LOG_INFO;
 
 #define ALIGN(size, align) ((size + align - 1) & (~(align - 1)))
 
-#define UVC_VERSION "SDK V1.20"
+#define UVC_VERSION "SDK V1.21"
 
 int app_quit = 0;
 void sigterm_handler(int sig) {
