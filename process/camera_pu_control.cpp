e6b43e79e07567a2e9f40de4e356804903a614e9
wengtao <tao.weng@rock-chips.com>
2021-08-10 16:31:58 +0800

    Exposure: implementation set_exposure_time & set_ae_mode
    Signed-off-by: wengtao <tao.weng@rock-chips.com>
    Change-Id: Ie3c0c34b383099fa14090dd654ef8f2687dcfc2a

--- process/camera_pu_control.cpp
+++ process/camera_pu_control.cpp
@@ -304,16 +304,45 @@ extern "C" int video_record_set_roll_mode(int mode) {
    return 0;
 }
 extern "C" int video_record_set_exposure_time(int time) {
-   //todo
+   char *ret = NULL;
+   char *table = TABLE_IMAGE_EXPOSURE;
+   struct json_object *js = NULL;
+   js = json_object_new_object();
+   if (NULL == js)
+   {
+      LOG_DEBUG("+++new json object failed.\n");
+      return -1;
+   }
+   LOG_DEBUG("exposure time = %d\n", time);
+   json_object_object_add(js, "sGainMode", json_object_new_string("auto"));
+   char str[8];
+   sprintf(str, "%f", ((float)time / 10000));
+   json_object_object_add(js, "sExposureTime", json_object_new_string(str));
+   ret = dbserver_media_set(table, (char *)json_object_to_json_string(js), 0);
+   json_object_put(js);
+   dbserver_free(ret);
    return 0;
 }
 extern "C" int video_record_set_ae_mode(int mode) {
-   if ( mode == 2){
-       LOG_DEBUG("AE:set auto mode!!\n");
-   }else {
-       LOG_DEBUG("AE:set  manual mode!!\n");
+   char *ret = NULL;
+   char *table = TABLE_IMAGE_EXPOSURE;
+   struct json_object *js = NULL;
+   js = json_object_new_object();
+   if (NULL == js)
+   {
+      LOG_DEBUG("+++new json object failed.\n");
+      return -1;
    }
-   //todo
+   if ( mode == 2){
+      LOG_DEBUG("AE:set auto mode!!\n");
+      json_object_object_add(js, "sExposureMode", json_object_new_string("auto"));
+   }else {
+      LOG_DEBUG("AE:set  manual mode!!\n");
+      json_object_object_add(js, "sExposureMode", json_object_new_string("manual"));
+   }
+   ret = dbserver_media_set(table, (char *)json_object_to_json_string(js), 0);
+   json_object_put(js);
+   dbserver_free(ret);
    return 0;
 }
 #endif
